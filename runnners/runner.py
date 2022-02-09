8dcf8918a03e750dd11f80067beb4a4f0a080f2b
Rashaad Cassim <rashcassim@gmail.com>
2022-02-09 17:46:20 +0200

    Corrected minor error

--- runnners/runner.py
+++ runnners/runner.py
@@ -17,7 +17,7 @@ supported_frameworks = ['tf', 'docker']
 
 docker_actions = ['build', 'run']
 terraform_actions = ['validate', 'fmt', 'apply', 'plan', 'state', 'destroy', 'init']
-all_action =[docker_actions, terraform_actions]
+all_action =docker_actions + terraform_actions
 
 docker_targets = ['experiment_setup_only', 'experiment_complete']
 
@@ -30,7 +30,6 @@ my_parser.add_argument('--action', action='store', choices= all_action, required
 my_parser.add_argument('--target', action='store', choices= docker_targets, required=False)
 args = my_parser.parse_args()
 
-print(args)
 if args.framework == 'docker':
     if args.action.lower() == 'build':
         os.system(run_commands[args.action])
@@ -41,7 +40,6 @@ if args.framework == 'tf':
     if args.action in docker_actions:
         message = "{action} can only be used with Docker relate commands".format(action = args.action)
         my_parser.error(message)
-    if args.action == 'validate':
         # TODO Add check to see what images are around
-        command_to_run = "docker run experiment_setup validate"
-        os.system(command_to_run)
+    command_to_run = "docker run experiment_setup {action}".format(action = args.action)
+    os.system(command_to_run)
