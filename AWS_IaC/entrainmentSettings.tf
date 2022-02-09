516de3a67ac3675e09456780e71b2b07163563dc
Rashaad Cassim <rashcassim@gmail.com>
2022-02-09 17:40:18 +0200

    Added back needed code
    Added back necessary code for S3 bucket creation while the lambda is
    still in limbo.

--- AWS_IaC/entrainmentSettings.tf
+++ AWS_IaC/entrainmentSettings.tf
@@ -4,16 +4,16 @@
 ############ entrainmentLambda ##########
 #########################################
 
-# resource "random_pet" "lambda_bucket_name" {
-#   length = 4
-# }
+resource "random_pet" "lambda_bucket_name" {
+  length = 4
+}
 
-# resource "aws_s3_bucket" "lambda_bucket" {
-#   bucket = random_pet.lambda_bucket_name.id
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
 
-#   acl           = "private"
-#   force_destroy = true
-# }
+  acl           = "private"
+  force_destroy = true
+}
 
 
 # data "archive_file" "lambda_entrainment_controller" {
