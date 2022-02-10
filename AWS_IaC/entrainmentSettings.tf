4dde70a6d55197905163579afafc3ac37cc364b9
Rashaad Cassim <rashcassim@gmail.com>
2022-02-10 10:01:01 +0200

    Refactored common lamba s3 bucket
    Refactored common code to setup s3 bucket for lambda code

--- AWS_IaC/entrainmentSettings.tf
+++ AWS_IaC/entrainmentSettings.tf
@@ -4,18 +4,6 @@
 ############ entrainmentLambda ##########
 #########################################
 
-resource "random_pet" "lambda_bucket_name" {
-  length = 4
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
-
-  acl           = "private"
-  force_destroy = true
-}
-
-
 # data "archive_file" "lambda_entrainment_controller" {
 #   type = "zip"
 
