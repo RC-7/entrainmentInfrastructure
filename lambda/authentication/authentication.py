7bbf89356c716c0e2c79c1105ec5637ad43bdcc3
Rashaad Cassim <rashcassim@gmail.com>
2022-02-11 12:08:17 +0200

    Debugged authh lambda issues
    Debugged and tested auth lambda when creating a participant.

--- lambda/authentication/authentication.py
+++ lambda/authentication/authentication.py
@@ -3,14 +3,17 @@ import json
 import os
 import hashlib
 from base64 import b64encode
+import datetime
+import uuid
 
 TABLE_NAME = os.getenv('tableName')
 
 dynamodb = boto3.resource('dynamodb')
-EXPERIMEN_TABLE = dynamodb.Table(TABLE_NAME)
+EXPERIMENT_TABLE = dynamodb.Table(TABLE_NAME)
 
 def any_and_not_all (iterable):
-    return any(iterable and not all(iterable))
+    print(iterable)
+    return any(iterable) and not all(iterable)
 
 def respond(code, body):
     return {
@@ -30,13 +33,13 @@ def get_participant_id(hash):
     fe = buildFilterExpression(hash)
 
     # TODO pull into common util file
-    response = EXPERIMEN_TABLE.scan(FilterExpression = eval(fe), ProjectionExpression = prijection_attriibute)
+    response = EXPERIMENT_TABLE.scan(FilterExpression = eval(fe), ProjectionExpression = prijection_attriibute)
 
     participant_count = response['Count']
     participant_ID = response['Items']
 
     while 'LastEvaluatedKey' in response:
-        response = EXPERIMEN_TABLE.query(FilterExpression = eval(fe), ProjectionExpression = prijection_attriibute)
+        response = EXPERIMENT_TABLE.query(FilterExpression = eval(fe), ProjectionExpression = prijection_attriibute)
         participant_ID.append(response['Items'])
         participant_count += response['Count']
 
@@ -46,6 +49,12 @@ def get_participant_id(hash):
         return [False, 0]
     elif participant_count > 1:
         return [False, -1]
+
+def create_participant_table_entry(participant_info):
+
+    response = EXPERIMENT_TABLE.put_item(
+        Item = participant_info
+    )
 
 def buildFilterExpression (hash):
     fe = "Key('hashIdentifier').eq('" + hash +"')"
@@ -62,14 +71,31 @@ def check_id(secret_key, name):
     hash_value = create_hash(secret_key, name)
     return get_participant_id(hash_value)
 
-def email_secret_key(key):
+def email_secret_key(key, email):
+    print(key)
+    print(email)
+    # SES Stuff
     pass
 
 def create_participant(name, email):
     key_encoded = os.urandom(5)
-    secret_key = b64encode(key_encoded).decode('utf-8')
+    secret_key = b64encode(key_encoded)
+    encoded_name = bytes(name, 'utf-8')
+    participant_hash = create_hash(secret_key, encoded_name)
+    
+    participant_ID = str(uuid.uuid4())
 
-    status = email_secret_key(secret_key)
+    participant_info = {
+        'participantID': participant_ID,
+        'timestamp': str(datetime.datetime.now()),
+        'hashIdentifier': participant_hash
+    }
+    create_participant_table_entry(participant_info)
+    
+    secret_key_string = str(secret_key.decode('utf-8'))
+    status = email_secret_key(secret_key_string, email)
+
+    return [True, participant_ID]
 
 def create_negative_response_body(message, resonse_body):
     resonse_body['participant_ID'] = -1
@@ -77,6 +103,10 @@ def create_negative_response_body(message, resonse_body):
 
 def lambda_handler(event, context):
     requestValues =  json.loads(event['body'])
+    
+    for key in requestValues:
+        print(key)
+        print(requestValues[key])
     response_message = {
         'participant_ID': '',
         'message': ''
@@ -98,6 +128,7 @@ def lambda_handler(event, context):
                 create_negative_response_body('No participant ID found for name and secret key provided', response_message)
             if participantID == -1:
                 create_negative_response_body('Multiple participant ID\'s found', response_message)
+                
     if any_and_not_all (key in requestValues for key in ('secret_key','name')) and not \
         'email' in requestValues.keys():
             create_negative_response_body('Not enough info given to querry participant ID', response_message)
--- tst/end_to_end/auth.py
+++ tst/end_to_end/auth.py
@@ -4,13 +4,14 @@ import json
 def valid_auth_request(lambda_url):
     header_values = {
         'x-api-key' : '',
-        'Content-Type': 'application/json'
+        # 'Content-Type': 'application/json'
     }
     body_values = {
         'name': 'test_name',
         'email': 'testEmail@domain.com'
     }
-    response = requests.post(lambda_url, headers=header_values)
+    response = requests.post(lambda_url, headers=header_values, data=json.dumps(body_values))
+
     print('---------------------')
     print('Valid request response: ')
     print(response.text)
@@ -26,6 +27,7 @@ def send_invalid_auth_request(lambda_url):
 
 def main():
     f = open('AWS_IaC/aws_resources.json')
+    name = 'yes'
     aws_resources = json.load(f)
     valid_auth_request(aws_resources['authentication_lambda_url'])
 
