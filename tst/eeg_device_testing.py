b60e22ffc5ec517d8f05caa5414c5d50b655f37c
Rashaad Cassim <rashcassim@gmail.com>
2022-03-01 08:35:30 +0200

    Adding basic test runnner for eeg device

---
+++ tst/eeg_device_testing.py
@@ -0,0 +1,35 @@
+from testing_interface.eeg_device_interface import EEGDeviceInterface
+
+
+def main():
+    user_input = ''
+
+    gtec_device = EEGDeviceInterface()
+
+    while user_input != 'exit':
+        print("Enter action")
+        user_input = input()
+        if user_input == 'print':
+            gtec_device.print_all_device_info()
+        elif user_input == 'impedance':
+            impedance = gtec_device.impedance_check()
+            print(impedance)
+            with open('filename.txt', 'w') as f:
+                print(impedance, file=f)
+                print('Type: ')
+                print(type(impedance), file=f)
+        elif user_input == 'data':
+            print("Enter a filename")
+            filename = input()
+            print("Enter the number of cycles to run")
+            number_of_cycles = input()
+            gtec_device.get_data(int(number_of_cycles))
+            gtec_device.save_active_data_to_file(filename)
+        elif user_input == 'display':
+            gtec_device.display_data()
+
+    gtec_device.close_device()
+
+
+if __name__ == '__main__':
+    main()
